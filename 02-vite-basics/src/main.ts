e4e3abd2acebd9afd610dbed498aa7e5081cd2d9
Johan Nordström <johan@digitalvillage.se>
2024-03-19 21:33:16 +0100

    Function return-types and typing of arrays.

--- 02-vite-basics/src/main.ts
+++ 02-vite-basics/src/main.ts
@@ -25,3 +25,34 @@ greet("Johan", 42);
 greet("Script-kiddo", 4);
 greet("Charlie");
 // greet("David", false);
+
+const fancyGreet = (name = "Anonymous", age?: number) => {
+	if (!age) {
+		return `Oh hello there good sir/ma'am, ${name}!`;
+	}
+	return `Oh hello there good sir/ma'am, ${name}! You look splendid for being ${age} years old.`;
+}
+
+const msg = fancyGreet("Johan", 1337);
+//     ^?
+
+fancyGreet();
+fancyGreet("Harold");
+fancyGreet(undefined, 42);
+// fancyGreet(null, 50);
+
+const pets = ["cat", "dog", "hamster", "goldfish"];
+//      ^?
+pets.push("rabbit");
+
+pets.forEach(pet => {
+//             ^?
+	console.log(pet.toLocaleUpperCase());
+});
+
+const ages = [1, 3, 5, 7, 13, 21];
+//      ^?
+ages.forEach(age => {
+//            ^?
+	// console.log(age.toLocaleUpperCase());  // Property 'toLocaleUpperCase' does not exist on type 'number'.
+});
