c3b74b15254649d3900d5b214d78ba6d23f3569b
Johan Nordström <johan@digitalvillage.se>
2024-03-26 21:18:25 +0100

    Added support for ID on our Todos.

--- 03-todos/src/main.ts
+++ 03-todos/src/main.ts
@@ -4,14 +4,15 @@ const todosEl = document.querySelector<HTMLUListElement>("#todos")!;
 const newTodoFormEl = document.querySelector<HTMLFormElement>("#new-todo-form")!;
 
 interface Todo {
+	id: number;
 	title: string;
 	completed: boolean;
 }
 
 const todos: Todo[] = [
-	{ title: "Learn about Everyday Types", completed: true },
-	{ title: "Learn about DOM Manipulation", completed: true },
-	{ title: "Learn about typing fetch/axios responses", completed: false },
+	// { id: 1, title: "Learn about Everyday Types", completed: true },
+	// { id: 2, title: "Learn about DOM Manipulation", completed: true },
+	// { id: 3, title: "Learn about typing fetch/axios responses", completed: false },
 ];
 
 // Render todos
@@ -29,7 +30,13 @@ newTodoFormEl.addEventListener("submit", (e) => {
 
 	const newTodoTitleEl = document.querySelector<HTMLInputElement>("#new-todo-title")!;
 
+	// Find the highest ID and add 1
+	const todoIds = todos.map(todo => todo.id); // [1, 2, 3]
+	const maxId = Math.max( 0, ...todoIds );   // Math.max( 1, 2, 3 )
+
+	// Create the todo and push it into the array 🫸🏻
 	todos.push({
+		id: maxId + 1,
 		title: newTodoTitleEl.value,
 		completed: false,
 	});
