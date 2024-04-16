1b341cac93cfa92749ed5803cf02e4901493b4dd
Johan Nordström <johan@digitalvillage.se>
2024-04-16 19:14:14 +0200

    "Catch all" with index signatures.

--- 04-async-todos/src/todo.types.ts
+++ 04-async-todos/src/todo.types.ts
@@ -2,9 +2,23 @@ export interface Todo {
 	id: number;
 	title: string;
 	completed: boolean;
+	[key: string]: any;
 }
 
 export type CreateTodoData = Omit<Todo, "id">;
 
 export type UpdateTodoData = Partial<Todo>;
 //           ^?
+
+const fakedTodo: Todo = {
+	id: 1337,
+	title: "My fake todo",
+	completed: false,
+	project_manager: "Bob",
+}
+const secondFakedTodo: Todo = {
+	id: 1337,
+	title: "My fake todo",
+	completed: false,
+	client_id: 42,
+}
