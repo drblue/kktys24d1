4b644966148cbae6e42e5b81ddb94ed90079daa1
Johan Nordström <johan@digitalvillage.se>
2024-04-23 19:06:25 +0200

    Added state and fetching of Todos.

--- 06-react-todos/src/pages/TodosPage.tsx
+++ 06-react-todos/src/pages/TodosPage.tsx
@@ -1,19 +1,38 @@
-import ListGroup from 'react-bootstrap/ListGroup'
+import { useEffect, useState } from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+import { getTodos as TodosAPI_getTodos } from "../services/TodosAPI";
+import { Todo } from "../types/Todo.types";
 
 const TodosPage = () => {
+	const [todos, setTodos] = useState<Todo[] | null>(null);
+
+	// Fetch todos when App is being mounted
+	useEffect(() => {
+		const getTodos = async () => {
+			const data = await TodosAPI_getTodos();
+			setTodos(data);
+		}
+		getTodos();
+	}, []);
+
 	return (
 		<>
 			<h1 className="mb-3">Todos</h1>
 
 			<p>Here be form</p>
 
-			<ListGroup className="todolist">
-				<ListGroup.Item
-					className={''}
-				>
-					This is my todo
-				</ListGroup.Item>
-			</ListGroup>
+			{todos && todos.length > 0 && (
+				<ListGroup className="todolist">
+					{todos.map(todo => (
+						<ListGroup.Item
+							className={''}
+							key={todo.id}
+						>
+							{todo.title}
+						</ListGroup.Item>
+					))}
+				</ListGroup>
+			)}
 		</>
 	)
 }
